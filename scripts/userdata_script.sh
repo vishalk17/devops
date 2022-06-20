1cd6701fc70b2840b3f9d3fa4002d1b40002f07d
vishalk17 <vishalkapadi95@gmail.com>
2022-06-20 05:46:43 -0700

    userdata_scirpt.sh : fix typo

--- scripts/userdata_script.sh
+++ scripts/userdata_script.sh
@@ -3,10 +3,10 @@
 #set debug mode
 set +x
 
+sudo mkdir -p /userdata_log
+
 # logs of userdata save 
 exec > >(tee /userdata_log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
-
-sudo mkdir -p /userdata_log
 
 echo  " update and fetch existing packages and install httpd server"
 sudo yum update -y
@@ -18,6 +18,8 @@ sed -i "s#Listen 80#Listen 90#g" /etc/httpd/conf/httpd.conf
 #add data to index.html
 echo " hi this is vishalk17" >> /var/www/html/index.html
 
-echo "apache httpd server on and automatic on if instance restart"
-sudo service httpd on
+echo "apache httpd server start"
+sudo service httpd start
+
+echo "automatic on if instance restart"
 sudo chkconfig httpd on
