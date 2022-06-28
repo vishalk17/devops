bc5bc08576c30ac1c9b131986d76e0e7b2c21663
vishalk17 <vishalkapadi95@gmail.com>
2022-06-28 12:37:33 +0530

    Update use_break.sh

--- scripts/use_break.sh
+++ scripts/use_break.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+########################
+
 for input in {1..10}
 do
         if [ $input -eq 5 ]
@@ -9,4 +11,20 @@ do
                 break   # break or stop the loop
         fi
         echo $input
+done
+
+###########################
+
+for (( a=1; a < 10; a++ ))
+do
+        echo "current value of outer loop a :  $a"
+        for (( b=1; b < 8; b++ ))
+        do
+        echo "current value of inner loop b : $b"
+                if [[ $b -eq 5 ]]
+                then
+                echo " b value is equal to 5  break the loop"
+                break 2
+                fi
+        done
 done
