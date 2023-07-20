e18bfbba79621c691139fe8937dc64fe257d9f57
vishalk17 <vishalkapadi95@gmail.com>
2023-07-20 20:40:14 +0530

    kubernets: notes on Nginx deployment
    - use of nginx ingress controller
    - use of metallb
    - deploy nginx, export service to the external world, pv, pvc, deployment

---
+++ kubernetes/nginx-deployment-k8s/src/nginx-start.sh
@@ -0,0 +1,5 @@
+kubectl apply -f nginx-pv.yaml
+kubectl apply -f nginx-pvc.yaml
+kubectl apply -f nginx-ingress.yaml
+kubectl apply -f nginx-service.yaml
+kubectl apply -f nginx-deployment.yaml
---
+++ kubernetes/nginx-deployment-k8s/src/nginx-stop.sh
@@ -0,0 +1,5 @@
+kubectl delete -f nginx-deployment.yaml
+kubectl delete -f nginx-service.yaml
+kubectl delete -f nginx-ingress.yaml
+kubectl delete -f nginx-pvc.yaml
+kubectl delete -f nginx-pv.yaml
